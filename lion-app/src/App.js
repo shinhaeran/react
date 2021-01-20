2ca7cdcf4b69eab624d6a90281142a93ed1ca31f
shinhaeran <haeran97@naver.com>
2021-01-21 02:08:39 +0900

    lifecycle, 토글버튼

--- lion-app/src/App.js
+++ lion-app/src/App.js
@@ -30,15 +30,20 @@ class WorldClock extends React.Component{
       minute:0,
       stop:false,
     }
-    this.a()
-    // this.timer = setInterval(() => {
-    //   this.setState((state)=>(
-        
-    //     state.minute === 59
-    //     ?{hour : state.hour+1,minute:0}
-    //     :{minute:state.minute+1}
-    //   ))
-    // }, 100)
+    console.log("  child 시작")
+  }
+  componentDidMount(){
+    this.a() // 이거를 constructor에 놓지말고 여기에 놓음
+    console.log("  child 마운트됨")
+  }
+
+  componentDidUpdate(){
+    console.log("  child 업데이트됨")
+  }
+
+  componentWillUnmount(){
+    console.log("  child 언마운트됨")
+    clearInterval(this.timer)
   }
   a(){
     this.timer = setInterval(() => {
@@ -48,7 +53,7 @@ class WorldClock extends React.Component{
         ?{hour : state.hour+1,minute:0}
         :{minute:state.minute+1}
       ))
-    }, 100)  
+    }, 5000)
   }
 
   handlingClick = (event) => { //handlingClick은 왠만하면 arrow func으로 작성해주는게 좋다
@@ -85,25 +90,38 @@ class App extends React.Component {
       ['시드니',12],
     ]
     this.state = {
-      content:''
+      content:'',
+      donottouch:true
     }
+    console.log("parent 시작")
   }
+  componentDidMount(){
+    console.log("parent 마운트됨")
+  }
+  componentDidUpdate(){
+    console.log("parent 업데이트됨")
+  }
+
   handlingChange = (event)=>{
     this.setState({content:event.target.value})
+  }
+  handlingClick = (event)=>{
+    this.setState((prevState)=>({donottouch:!prevState.donottouch}))
   }
   
 
   render(){
+    console.log("parent 렌더링 시작")
     return (
       <div className="App">
-        {/* <h1 style={{color:'red'}}>gkdlfn</h1>  */}
-        {/* <h1 style={myStyle}>gkdlfn</h1>  */}
         <h1 className={'myStyle'}>gkdlfn</h1> 
         <div className={'post'}>
           첫 게시글입니다.
           <textarea value={this.state.content} onChange={this.handlingChange}></textarea>
         </div>
-        {this.cityTimeData.map((citytime,index)=>
+        <button onClick={this.handlingClick}>누르지 마세요</button>
+        {this.state.donottouch&&  //donottouch state가 true일때만 child 컴포넌트가 보인다! 짱신기
+        this.cityTimeData.map((citytime,index)=>
           <WorldClock city={citytime[0]} time={citytime[1]} key={index}/>  
         )}
       </div>
@@ -158,4 +176,53 @@ fucntion component : state를 사용 못함 -> 쓰려면 class componenet로 써
 props나 state의 값이 바뀌면 그 컴포넌트의 render함수가 다시 호출된다.
 render함수가 가지고 있는 하위의 컴포넌트들의 각 render함수도 다시 호출된다.
 즉 props나 state의 값이 바뀌면 화면이 다시 그려진다. 오잉 뭐지
+*/
+
+/*event handling 맹글기
+1. 필요한 state를 만든다
+2. 핸들링 함수를 만들어 준다
+3. 만든거 태그 안에 On[이벤트] = {'[함수이름]'}
+
+/*
+lifecycle 순서: constructor->render->mount->re-render->update->unmount
+parent 시작 -> parent :constructor
+parent 렌더링 시작 -> parent :render
+  child 시작 -> child : constructor,render
+  child 마운트됨 ->child : mount -> componentDidMount()
+  parent 마운트됨 -> parent : mount -> componentDidMount()
+  
+update는 언제 되느냐? -> state와 prop이 변할 때
+
+parent 시작
+parent 렌더링 시작
+   child 시작
+   child 마운트됨
+parent 마운트됨
+   child 업데이트됨 -> re-render하고 update -> componentDidUpdate
+parent 렌더링 시작 -> 부모의 update 시, 모든 자식들 컴포넌트들 re-render, update해줌
+   child 업데이트됨
+parent 업데이트됨
+
+->부모 업데이트마다 모든 자식들 렌더,업데이트 다 해줘야 하는게 너무 비효율적이다
+부모 업데이트 해도 자식들 업데이트x->shouldComponent(모든 업데이트 마다 커스텀 가능), pureComponent(미리 알아서 제공해주는 기능들이 많당)
+*/
+
+
+/*
+constructor
+1. state 구조 설정
+2. 컴포넌트가 mount하기 전에 할 설정
+3. setState x -> 권장
+
+componentDidMount
+1.필요한 데이터 요청
+2. 각종 비동기 요청
+
+componentDidUpdate
+1. 업데이트 이후 수정할 때
+2. if() {setState()} -> 왠만하면 if로 달아주자 -> 무한으로 setstate될까봐 -> 그럼 또 업데이트 판정
+
+componentWillUnmount
+1. 데이터 요청, 비동기함수, 타이머 종료할 때 
+2. setstate x
 */
