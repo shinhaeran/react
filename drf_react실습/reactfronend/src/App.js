a733941d782bcf7ed7ba75bf3ff9d793a6cc43d9
shinhaeran <haeran97@naver.com>
2021-01-23 01:54:27 +0900

    post delete, material-ui 꾸미기1

--- drf_react실습/reactfronend/src/App.js
+++ drf_react실습/reactfronend/src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './App.css';
 import api from './api';
 import PostView from './Components/PostView'
+//ui
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
 
 class App extends React.Component{
   constructor(props){
@@ -35,36 +42,44 @@ class App extends React.Component{
     event.preventDefault() //event의 새로고침,페이지 맨 위로 올리는 등의 디폴트 기능 -> 막는 기능
     let result = await api.createPost({title:this.state.title, content:this.state.content})
     console.log("완료됨",result) 
+    this.setState({title:'',content:''}) // 
+    this.getPost()
+  }
+
+  handlingDelete = async(event) => {
+    await api.deletePost(event.target.value)
+    this.getPost()
   }
 
   render(){
     return (
       <div className="App">
-        <div className="PostingSection">
-        <h2>대나무 숲 글 작성하기</h2>
-        <form onSubmit = {this.handlingSubmit}>
-        <input 
-          name = "title"
-          value = {this.state.title}
-          onChange={this.handlingChange}
-        />
-        <textarea 
-          name = "content"
-          value = {this.state.content}
-          onChange={this.handlingChange}
-        />
-        <button type="submit">제출하기</button>
-        </form>
-        </div>
-        <div className="ViewSection">
-          {
-            this.state.results.map((post)=>
-            <PostView key={post.id} id={post.id} title={post.title} content={post.content}/>)
-          }
-          <PostView/>
-        </div>
+        <Container maxWidth="lg">
+          <div className="PostingSection">
+          <Paper className="Postingpaper">
+            <h2>대나무 숲 글 작성하기</h2>
+            <form className="Postingform" onSubmit = {this.handlingSubmit}>
+            <TextField id="outlined-textarea" variant="outlined" name = "title" value = {this.state.title} label="Title" onChange={this.handlingChange}/>
+            <TextField id="outlined-multiline-static" variant="outlined" rows={3} multiline name = "content" value = {this.state.content} label="Content" onChange={this.handlingChange}/>
+            
+            <Button variant="contained">제출하기</Button>
+            </form>
+          </Paper>
+          </div>
+          <div className="ViewSection">
+            {
+              this.state.results.map((post)=>
+              <div>
+                <PostView key={post.id} id={post.id} title={post.title} content={post.content}/>
+                <Button variant="contained" color="primary" value={post.id} onClick={this.handlingDelete}>지우기</Button>
+                
+              </div>
+              )
+            }
+            <PostView/>
+          </div>
         
-        
+      </Container>
       </div>
     );
   }
--- drf_react실습/reactfronend/src/api.js
+++ drf_react실습/reactfronend/src/api.js
@@ -13,5 +13,9 @@ export default{
     createPost(data){
         return axios.post('/posts/',data)
     },
+
+    deletePost(id){
+        return axios.delete('/posts/'+String(id))
+    },
 }
 
